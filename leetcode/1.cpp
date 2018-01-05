665e7c9b67fa7ce7fe462041db9b7e6d48051798
KINSANG CHING <chengjsh@mail2.sysu.edu.cn>
2018-01-04 22:59:14 -0500

    Update 1.cpp

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -1,1 +1,18 @@
-#  reserved 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> dict;
+        vector<int> res;
+        for (unsigned int i = 0; i < nums.size(); i++) {
+            int rest = target - nums[i];
+            map<int, int>::const_iterator it = dict.find(rest);
+            if (it == dict.end()) {
+                dict[nums[i]] = i;
+            } else {
+                res.push_back(it->second);
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
