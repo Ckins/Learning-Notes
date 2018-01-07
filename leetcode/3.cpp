0d18200b37b9dc7affbc2d190b21e74edf5b46fd
KINSANG CHING <chengjsh@mail2.sysu.edu.cn>
2018-01-06 23:47:53 -0500

    Update 3.cpp

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,1 +1,21 @@
-# reserved
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() == 0 || s.length() == 1)
+            return s.length();
+        int result = 1;
+        //value, index
+        map<char, int> letter;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (letter.find(s[i]) == letter.end() || letter[s[i]] < start) {
+                result = max(result, i - start + 1);
+            }
+            else {
+                start = letter[s[i]];
+            }
+            letter[s[i]] = i + 1;
+        }
+        return result;
+    }
+};
