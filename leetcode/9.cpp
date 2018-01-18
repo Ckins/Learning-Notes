c2c13acff66a58afe8062b680a0e5fa71d3e7bf3
ckins <492159680@qq.com>
2018-01-17 23:51:28 -0500

    9.cpp

--- leetcode/9.cpp
+++ leetcode/9.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x != 0 && x % 10 == 0) return false;
+    
+        int half_sum = 0;
+        while (x > half_sum) {
+            half_sum = half_sum * 10 + x % 10;
+            x /= 10;
+        }
+        return (half_sum == x) || (half_sum / 10 == x);
+    }
+};
