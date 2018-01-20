96091839f7b563422e9175f7c9a99653d9ef9de2
ckins <492159680@qq.com>
2018-01-20 08:55:11 -0500

    14

--- leetcode/14.cpp
+++ leetcode/14.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) return string("");
+        if (strs.size() == 1) return strs[0];
+        for (auto &s : strs) {
+            if (s.size() == 0) return string("");
+        }
+        string base = strs[0];
+        for (unsigned int i = 1; i < strs.size(); i++) {
+            base = longestPrefixInTwoStr(base, strs[i]);
+        }
+        return base;
+    }
+    string longestPrefixInTwoStr(string a, string b) {
+        if (a.size() == 0 || b.size() == 0) return string("");
+        unsigned int i = 0;
+        for (; i < a.size(); i++) {
+            if (a[i] != b[i]) {
+                break;
+            }
+        }
+        return a.substr(0, i);
+    }
+};
